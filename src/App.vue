e545b2059e9f21ce4467b71acf1ed9402e4eb11f
WUYUEZONG <w.yzong@outlook.com>
2023-07-27 17:32:47 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <div>logo</div>
+    <div> menus </div>
+    <div> login </div>
+  </header>
+
+  <main>
+    <div class="content">
+      <span>Ondeck is your remote</span>
+      <span>conference calling tool</span>
+      <div>
+        Ondeck is a service that allows you to create beautiful, online, and encrypted video calls for you and your remote team.
+      </div>
+    </div>
+    <div>
+      <img src="/src/Assets/Hero Image.png" alt="" />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+</style>
